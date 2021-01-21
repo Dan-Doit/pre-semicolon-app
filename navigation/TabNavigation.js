8a62127799b11e1f1ffd2337a570f0023f3ade95
Dan <spqjeks@naver.com>
2021-01-21 20:39:18 +0900

    ready to build app with navigations

---
+++ navigation/TabNavigation.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+import { View } from "react-native";
+import Home from "../screens/Home";
+import Search from "../screens/tabs/Search";
+import Notifications from "../screens/tabs/Notifications";
+import Profile from "../screens/tabs/Profile";
+import MessagesLink from '../components/MessagesLink';
+
+const stackFactory = (initialRoute, customConfig) =>
+  createStackNavigator({
+    InitialRoute: {
+      screen: initialRoute,
+      navigationOptions: { ...customConfig }
+    }
+  });
+
+export default createBottomTabNavigator({
+  Home: {
+    screen: stackFactory(Home, {
+      title: "Home",
+      headerRight: (
+        <MessagesLink />
+      )
+    })
+  },
+  Search: {
+    screen: stackFactory(Search, {
+      title: "Search"
+    })
+  },
+  Add: {
+    screen: View,
+    navigationOptions: {
+      tabBarOnPress: ({ navigation }) => navigation.navigate("PhotoNavigation")
+    }
+  },
+  Notifications: {
+    screen: stackFactory(Notifications, {
+      title: "Notifications"
+    })
+  },
+  Profile: {
+    screen: stackFactory(Profile, {
+      title: "Profile"
+    })
+  }
+});
